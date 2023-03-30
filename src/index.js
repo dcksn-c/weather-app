dcaa8a3c8d76fec9bd3f0d947432c84101bb7d4d
Dickson <116993989+dcksn-c@users.noreply.github.com>
2023-03-30 23:24:58 +0800

    added loader while waiting for data to be fetched

--- src/index.js
+++ src/index.js
@@ -9,11 +9,15 @@ const form = document.querySelector("form");
 const weatherIcon = document.querySelector(".content img");
 const weatherDisplay = document.querySelector(".content");
 const errorMessage = document.querySelector(".error");
+const loader = document.querySelector(".loader");
 
 async function checkWeather(city) {
     try {
+        weatherDisplay.style.display = "";
+        loader.style.display = "block";
         const response = await fetch(`${apiUrl}${city}&key=${apiKey}`);
         const data = await response.json();
+        loader.style.display = "";
 
         cityName.textContent = data.location.name;
         temp.textContent = `${data.current.temp_c}°C`;
